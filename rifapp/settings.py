ba635e4a624e8702958e60a93e901bfd9c09cc4b
DeybisMelendez <damv2007@gmail.com>
2025-10-27 20:25:56 -0600

    agregado host permitido

--- rifapp/settings.py
+++ rifapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-15k0$8ys2k@m!cpx#_&ohx05rhsov-elprx!e96+rv74t1fk!y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
