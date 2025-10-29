2f957e7f3a0a7102aee75431b5c5da1c7bdc0ed2
DeybisMelendez <damv2007@gmail.com>
2025-10-29 12:17:12 -0600

    reducido copos de nieve una vez mas :V

--- static/snowstorm.js
+++ static/snowstorm.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   document.body.appendChild(snowContainer);
 
   const snowflakes = ["❅", "❆"];
-  const snowflakeCount = 10; // número total de copos en pantalla
+  const snowflakeCount = 5; // número total de copos en pantalla
 
   for (let i = 0; i < snowflakeCount; i++) {
     const snowflake = document.createElement("span");
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // posición y tamaño aleatorio
     snowflake.style.left = Math.random() * 100 + "vw";
     snowflake.style.fontSize = 0.8 + Math.random() * 1 + "rem";
-    snowflake.style.animationDuration = 5 + Math.random() * 10 + "s";
+    snowflake.style.animationDuration = 2 + Math.random() * 10 + "s";
     snowflake.style.animationDelay = Math.random() * 5 + "s";
     snowflake.style.opacity = 0.3 + Math.random() * 0.5;
 
